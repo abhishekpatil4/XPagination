76b75a7ce01a34bd765b17258e499a920f43bc82
Abhishek Patil <abishkpatil@gmail.com>
2024-05-09 23:02:21 +0530

    feat: add table structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import { useState } from 'react'
+import { Table } from './components/Table'
 // import './App.css'
 
 function App() {
-  return<>
-  <h1>Employee Data</h1>
+  return <>
+    <div>
+      <h1 style={{textAlign:'center'}}>Employee Data Table</h1>
+      <Table />
+    </div>
   </>
 }
 
---
+++ src/components/Table.jsx
@@ -0,0 +1,16 @@
+import "./table.css"
+export const Table = () =>{
+    return<>
+    <table style={{width:'100%', backgroundColor:'#009879'}}>
+        <thead style={{backgroundColor:'#009879', color:'white'}}>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Role</th>
+        </thead>
+        <tbody>
+            
+        </tbody>
+    </table>
+    </>
+}
