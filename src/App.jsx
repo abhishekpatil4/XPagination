b3442801b15740444fdd7e14c966b7c410960611
Abhishek Patil <abishkpatil@gmail.com>
2024-05-10 01:39:19 +0530

    fix: pagination bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
 import { useState } from 'react'
 import { Table } from './components/Table'
+import { useParams } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 // import './App.css'
 
 function App() {
+  const { page } = useParams();
+  const nagivate = useNavigate();
+  useEffect(() => {
+    if (page == undefined) {
+      nagivate("/1");
+    }
+  }, [])
   return <>
-    <div style={{display:'flex', flexDirection:'column', justifyContent:'center'}}>
-      <h1 style={{textAlign:'center'}}>Employee Data Table</h1>
-      <Table />
-    </div>
+    {page &&
+      <>
+        <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
+          <h1 style={{ textAlign: 'center' }}>Employee Data Table</h1>
+          <Table />
+        </div>
+      </>
+    }
   </>
 }
 
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -9,18 +9,18 @@ export const Table = () => {
     const [pageNum, setPageNum] = useState(Number(page));
     const navigate = useNavigate();
     const handleNext = () => {
-        if (Number(pageNum) < 5) {
-            navigate(`/${Number(pageNum)+1}`)
-            setPageNum((prev) => prev+1);
-        }else{
+        if (pageNum < 5) {
+            navigate(`/${pageNum + 1}`)
+            setPageNum((prev) => prev + 1);
+        } else {
             alert("You're already at last page");
         }
     }
     const handlePrev = () => {
-        if (Number(pageNum) > 1) {
-            setPageNum((prev) => prev-1);
-            navigate(`/${Number(pageNum)-1}`)
-        }else{
+        if (pageNum > 1) {
+            setPageNum((prev) => prev - 1);
+            navigate(`/${pageNum - 1}`)
+        } else {
             alert("You're already at first page");
         }
     }
@@ -29,12 +29,8 @@ export const Table = () => {
             try {
                 const API_URL = "https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json";
                 let res = await axios.get(API_URL);
-                if (page == undefined) {
-                    page = 0;
-                }
-                let pgNo = (Number(page) - 1) * 10;
+                let pgNo = (pageNum-1) * 10;
                 setData(res.data.slice(pgNo, pgNo + 10));
-
             } catch (e) {
                 console.log(e.response.status);
                 alert("Error: Unable to fetch data from API");
@@ -67,7 +63,7 @@ export const Table = () => {
         </table>
         <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
             <button className="pagination-element" onClick={handlePrev}>Previous</button>
-            <div className="pagination-element">{page ? page : '1'}</div>
+            <div className="pagination-element">{pageNum}</div>
             <button className="pagination-element" onClick={handleNext}>Next</button>
         </div>
     </>
